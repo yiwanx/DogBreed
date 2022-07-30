87e7ecc4d9f0b4d015caca15bad1dea5d84288c9
Ivan Petukhov <i.petukhov@octopod.ru>
2022-07-30 15:37:23 +0300

    inited rep

---
+++ DogBreed/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DogBreed
+//
+//  Created by Ivan Petukhov on 30.07.2022.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
